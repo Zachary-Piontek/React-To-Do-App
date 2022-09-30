56fb4a2e96ab3aa77016864992f27eda3bb3db8b
Zachary Piontek <zakpiontek@gmail.com>
2022-09-30 10:27:34 -0500

    added auth component and display page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
+import { Route } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth';
+import './components/auth.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Route path='/auth/:type' component={Auth} />
     </div>
   );
 }
---
+++ src/components/Auth.js
@@ -0,0 +1,38 @@
+import { useContext, useState } from 'react';
+import { NavLink, Redirect, useParams } from 'react-router-dom';
+import { UserContext } from '../hooks/userContext';
+import { userAuth } from '../services/auth';
+
+export default function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { user, setUser } = useContext(UserContext);
+  const { type } = useParams();
+
+  if (user) return <Redirect to='/todos' />;
+
+  const submitAuth = async () => {
+    const response = await userAuth(email, password, type);
+    setUser(response);
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <>
+      <div className='auth-page'>
+        <NavLink id='sign-in' to='/auth/sign-in'>Sign In</NavLink>
+        <NavLink id='sign-up' to='/auth/sign-up'>Sign Up</NavLink>
+        <form onSubmit={submitAuth}>
+          <label>
+            <input type='text' name='email' value={email} placeholder='email' onChange={e => setEmail(e.target.value)}></input>
+          </label>
+          <label>
+            <input type='text' name='password' value={password} placeholder='password' onChange={e => setPassword(e.target.value)}></input>
+          </label>
+          <input type='submit' value='submit'></input>
+        </form>
+      </div>
+    </>
+  );
+}
